0a5d5ba3b1e75205a379fa5534b0d792082dc7bb
kol1a_ <koljanmuzyka2108@gmail.com>
2023-11-23 22:22:58 +0200

    lol

--- Car_wash/CarForm.cpp
+++ Car_wash/CarForm.cpp
@@ -1,4 +1,5 @@
 #include "CarForm.h"
+#include <locale.h>
 
 using namespace Carwash;
 using namespace System;
@@ -6,6 +7,7 @@ using namespace System::Windows::Forms;
 
 [STAThreadAttribute]
 int main(array<String^>^ args) {
+	setlocale(LC_ALL, ".1251");
 	Application::SetCompatibleTextRenderingDefault(false);
 	Application::EnableVisualStyles();
 	Application::Run(gcnew CarForm());
